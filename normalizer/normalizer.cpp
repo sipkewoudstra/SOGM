4274feedba06d281da84f375c660dbfa1cd6fa06
Sipke <sipkewoudstra@gmail.com>
2014-10-31 00:49:44 +0100

    Almost working version, only problem with for loops.

--- normalizer/normalizer.cpp
+++ normalizer/normalizer.cpp
@@ -1,2 +1,15 @@
 #include "normalizer.h"
 
+int main()
+{
+	normalizer normalize;
+	normalize.setPath("/Users/sipkewoudstra/Desktop/Crap/Cowbell Little Sister.wav");
+	normalize.setOutputPath("/Users/sipkewoudstra/Desktop/Crap/Cowbell Little Sister Normalized.wav");
+	normalize.setPeakLevel(1);	
+	normalize.read();
+	normalize.printInfo();
+	normalize.getHardestPeak();
+	normalize.normalize();
+
+	return 0;
+}
--- normalizer/normalizerclass.cpp
+++ normalizer/normalizerclass.cpp
@@ -1,18 +1,65 @@
 #include "normalizer.h"
 
+void normalizer::setPeakLevel(float _peakLevel)
+{
+	peakLevel = _peakLevel;
+}
+
+void normalizer::setPath(string _path)
+{
+	path = _path;
+}
+void normalizer::setOutputPath(string _path)
+{
+	outputPath = _path;
+}
+
 void normalizer::read()
 {
-	input = sf_open(path.c_string, SFM_READ, &inputInfo);
+	input = sf_open(path.c_str(), SFM_READ, &inputInfo);
+	output = sf_open(outputPath.c_str(), SFM_WRITE, &inputInfo);
 }
-void normalizer::gethardestpeak()
+void normalizer::printInfo()
 {
+	frames = inputInfo.frames;
+	samplerate = inputInfo.samplerate;
+	channels = inputInfo.channels;
+	cout << "File " << path << " has " << frames << " frames, a samplerate of " << samplerate << " and " << channels << " channels." << endl;
+}
 
+void normalizer::getHardestPeak()
+{
+	int place = 0;
+	float buffer[buffersize*channels];
+	place = sf_readf_float(input, buffer, buffersize);
+	while(place != 0)
+	{
+	for(int i = 0; i < place; i++)
+		{
+			if(buffer[i] > hardestPeak)
+			{
+				hardestPeak = buffer[i];
+			}
+		}
+		place = sf_readf_float(input, buffer, buffersize);
+	}
+	cout << "The hardest peak is: " << hardestPeak << endl;
 }
 void normalizer::normalize()
 {
-
-}
-void normalizer::write()
-{
-	
+	gainFactor = peakLevel/hardestPeak;
+	float tempBuffer[buffersize*channels];
+	float gainedBuffer[buffersize*channels];
+	float inplace, out;
+	inplace = sf_readf_float(input, tempBuffer, buffersize);
+	while(inplace != 0)
+	{
+		for(int i = 0; i < inplace; i++)
+		{
+			gainedBuffer[i] = tempBuffer[i]*gainFactor;
+		}
+		out = sf_writef_float(output, gainedBuffer, buffersize);
+		inplace = sf_readf_float(input, tempBuffer, buffersize);
+	}
+	out = sf_writef_float(output, gainedBuffer, buffersize);
 }
