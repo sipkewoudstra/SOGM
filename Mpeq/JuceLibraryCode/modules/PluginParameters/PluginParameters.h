a6d5b3e4452a5f9bc5eac062f12690a460657da9
sipkewoudstra <sipkewoudstra@gmail.com>
2014-12-05 23:15:19 +0100

    Nothing Changed

--- Mpeq/JuceLibraryCode/modules/PluginParameters/PluginParameters.h
+++ Mpeq/JuceLibraryCode/modules/PluginParameters/PluginParameters.h
@@ -1,5 +1,5 @@
 // This is an auto-generated file to redirect any included
 // module headers to the correct external folder.
 
-#include "../../../../../JUCE-OSX/modules/PluginParameters/PluginParameters.h"
+#include "/Users/sipkewoudstra/Documents/Informatica/C++ /JUCE-OSX/modules/PluginParameters/PluginParameters.h"
 
--- Mpeq/JuceLibraryCode/modules/juce_core/juce_core.h
+++ Mpeq/JuceLibraryCode/modules/juce_core/juce_core.h
@@ -1,17 +1,5 @@
 // This is an auto-generated file to redirect any included
 // module headers to the correct external folder.
 
-#if defined (JUCER_XCODE_MAC_F6D2F4CF)
- #include "/Users/sipkewoudstra/Desktop/JUCE-OSX/modules/juce_core/juce_core.h"
-#elif defined (JUCER_VS2005_78A5003)
- #include "/Users/sipkewoudstra/Downloads/JUCE-OSX/modules/juce_core/juce_core.h"
-#elif defined (JUCER_VS2008_78A5006)
- #include "/Users/sipkewoudstra/Downloads/JUCE-OSX/modules/juce_core/juce_core.h"
-#elif defined (JUCER_VS2010_78A501D)
- #include "/Users/sipkewoudstra/Downloads/JUCE-OSX/modules/juce_core/juce_core.h"
-#elif defined (JUCER_LINUX_MAKE_7346DA2A)
- #include "/Users/sipkewoudstra/Downloads/JUCE-OSX/modules/juce_core/juce_core.h"
-#else
- #error "This file is designed to be used in an Introjucer-generated project!"
-#endif
+#include "/Users/sipkewoudstra/Documents/Informatica/C++ /JUCE-OSX/modules/juce_core/juce_core.h"
 
